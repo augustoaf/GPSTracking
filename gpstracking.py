d9c2b9803551ba030163fea682623bd8907da0b8
augusto.furtado <augusto.furtado@accenture.com>
2021-06-04 16:16:28 -0300

    bugfix - remove sleep() function in order to avoid serial readings in the past

--- gpstracking.py
+++ gpstracking.py
@@ -4,7 +4,7 @@ import datetime
 import sys
 import paho.mqtt.client as mqtt 
 
-serial_port = serial.Serial ("/dev/ttyS0")
+serial_port = serial.Serial ("/dev/ttyS0", timeout=1)
 gpgga_info = "$GPGGA,"
 
 def convert_to_degrees(raw_value):
@@ -17,8 +17,15 @@ def convert_to_degrees(raw_value):
     position = "%.4f" %(position)
     return position
 
-def write_to_file(is_new_file, content):
+def write_coordinates_to_file(is_new_file, content):
     file_name = "coordinates.txt"
+    write_to_file(is_new_file, content, file_name) 
+
+def write_gga_to_file(is_new_file, content):
+    file_name = "gga.txt"
+    write_to_file(is_new_file, content, file_name)
+
+def write_to_file(is_new_file, content, file_name):
     file = ""
     #parameter "a" will append new content at the end of the file, "w" will overwrite the previous file content - both will create a file if it does not exists.
     if is_new_file:
@@ -28,7 +35,7 @@ def write_to_file(is_new_file, content):
     content = content + "\n"
     file.write(content)
     file.close()
-    print(content)    
+    print(content)
 
 def instantiate_mqtt_client():
     client = ""
@@ -52,43 +59,48 @@ def send_message(mqtt_client, payload):
             write_to_file(False, error)
 
 try:
-    write_to_file(True, "")#create an empty log file
+    write_coordinates_to_file(True, "")#create an empty log file
+    write_gga_to_file(True, "")#create an empty log file
     mqtt_client = instantiate_mqtt_client()
-            
+    date_to_wait = datetime.datetime.now()
+
     while True:
         try:
             #read NMEA string received
             received_data = (str)(serial_port.readline()) 
-            #check for NMEA GPGGA string
-            GPGGA_data_available = received_data.find(gpgga_info)   
+            
+            if (datetime.datetime.now() > date_to_wait):
+                #check for NMEA GPGGA string
+                GPGGA_data_available = received_data.find(gpgga_info)   
 
-            if (GPGGA_data_available>0):
-                #read and store data after “$GPGGA,” string
-                GPGGA_buffer = received_data.split(gpgga_info,1)[1]  
-                NMEA_buff = (GPGGA_buffer.split(","))
-                nmea_time = []
-                nmea_latitude = []
-                nmea_longitude = []
-                nmea_time = NMEA_buff[0]                    
-                nmea_latitude = NMEA_buff[1]                
-                nmea_longitude = NMEA_buff[3]               
-                latitude = (float)(nmea_latitude)
-                latitude = convert_to_degrees(latitude)
-                longitude = (float)(nmea_longitude)
-                longitude = convert_to_degrees(longitude)
-                               
-                #write GPS coordinates in a file
-                current_date_and_time = datetime.datetime.now()
-                content = str(current_date_and_time) + "," + latitude + "," + longitude 
-                write_to_file(False, content)
-                #publish mqtt message
-                send_message(mqtt_client, content)
-
-                time.sleep(15)
+                if (GPGGA_data_available>0):
+                    #read and store data after “$GPGGA,” string
+                    GPGGA_buffer = received_data.split(gpgga_info,1)[1]  
+                    NMEA_buff = (GPGGA_buffer.split(","))
+                    nmea_time = []
+                    nmea_latitude = []
+                    nmea_longitude = []
+                    nmea_time = NMEA_buff[0]                    
+                    nmea_latitude = NMEA_buff[1]                
+                    nmea_longitude = NMEA_buff[3]               
+                    latitude = (float)(nmea_latitude)
+                    latitude = convert_to_degrees(latitude)
+                    longitude = (float)(nmea_longitude)
+                    longitude = convert_to_degrees(longitude)
+                                
+                    #write GPS coordinates in a file
+                    current_date_and_time = datetime.datetime.now()
+                    content = str(current_date_and_time) + "," + nmea_time + "," + latitude + "," + longitude 
+                    write_coordinates_to_file(False, content)
+                    write_gga_to_file(False, str(current_date_and_time) + "," + received_data)
+                    #publish mqtt message
+                    send_message(mqtt_client, content)
+                    date_to_wait = datetime.datetime.now() + datetime.timedelta(0,15)
         except Exception as e:
             error = f"GPS Coordinates: Logging exception as repr: {e!r}"
-            write_to_file(False, error)
-            time.sleep(5)
-
+            write_coordinates_to_file(False, error)
+            date_to_wait = datetime.datetime.now() + datetime.timedelta(0,5)
+            
 except KeyboardInterrupt:
+    serial_port.close()
     sys.exit(0)
